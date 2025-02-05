5f3386c002d3de8d0d5baebaa3f8fec1808d547a
hasanshrestha <hasan.xtha.5311@gmail.com>
2025-02-05 21:33:00 +0545

    user model, service and controller

--- src/middlewares/guard.ts
+++ src/middlewares/guard.ts
@@ -1,135 +1,95 @@
-// //import Boom from "@hapi/boom";
+const jwt = require("jsonwebtoken");
+const { user, role, setting } = require("@models");
 
-// import { SuccessResponse } from '@src/helpers';
-// import {
-//   InputUserInterface,
-//   UserInterface,
-//   //UserRequestInterface,
-// } from '@src/interfaces';
-// import { UserService } from '@src/services';
-// import { NextFunction } from 'express';
+import { UserInterface } from '@src/interfaces';
+import { UserService } from '@src/services';
+import { Request, Response, NextFunction } from 'express';
 
-// class Guard {
-//   private static instance: Guard;
 
-//   private constructor() { }
+class Guard {
+    private static instance: Guard;
+  
+    private constructor() { }
+  
+    static get(): Guard {
+      if (!Guard.instance) {
+        Guard.instance = new Guard();
+      }
+      return Guard.instance;
+    }
 
-//   static get(): Guard {
-//     if (!Guard.instance) {
-//       Guard.instance = new Guard();
-//     }
-//     return Guard.instance;
-//   }
+    private auth = async (token: string): Promise<UserInterface | undefined> => {
+      try {
+        const userExists = await new UserService().findByPk(1);      //GET ID AFTER DECODING TOKEN 
+        if (!userExists) {
+          throw Boom.unauthorized(`Auth failed`, token, {
+            message: `Token is invalid or user doesn't exits `,
+            path: "authorization",
+          });
+        }
+        // if (!userExists.emailVerified || !userExists.phoneNumberVerified) {
+        //   return this.verifyUser(userExists.id, token);
+        // }
+        return userExists;
+      } catch (error: any) {
+          throw Boom.unauthorized(`Auth failed`, token, {
+            message: `Auth failed `,
+            path: "authorization",
+          });
+      }
+    };
+  
+    public grantNext = async (req: Request, res: Response, next: NextFunction) => {
+      const token = req.headers.authorization as string;
+      if (!token) {
+        throw Boom.unauthorized("Auth failed", token, {
+          message: "Auth failed",
+          path: "authorization",
+        });
+      }
+      const user = await this.auth(token.replace("Bearer ", ""));
+      if (!user) {
+        throw Boom.unauthorized(`Auth failed`, token, {
+          message: `Auth Failed`,
+          path: "authorization",
+        });
+      }
+      req.user = user;
+      next();
+    };
+  
+}
+  
+const guard = Guard.get();
+  
+export { guard as Guard };
 
-//   auth = async (token: string): Promise<UserInterface | undefined> => {
-//     try {
-//       if (!token) {
-//         return res.status(401).json({ message: "No token, authorization denied" });
-//       }
-//       const userExists = await new UserService().findOne({
-//         sub: verify.sub,
-//       });
 
-//       return SuccessResponse.send({
-//         message: contextValue.i18nProvider.__("administratorLogin.success"),
-//         data: {
-//           token: {
-//             access: access,
-//             refresh: refresh,
-//           },
-//           role: roleExists,
-//         },
-//       });
-      
-//       if (!userExists) {
-//         throw new Error(`guard.auth.error`, {
-//           extensions: {
-//             code: 'UNAUTHENTICATED',
-//             argumentName: 'authorization',
-//             http: {
-//               status: 401,
-//             },
-//             data: {
-//               global: true,
-//               workspace: true,
-//             }
-//           },
-//         });
-//       }
-//       if (!userExists.emailVerified || !userExists.phoneNumberVerified) {
-//         return this.verifyUser(userExists.id, token);
-//       }
-//       return userExists;
-//     } catch (error: any) {
-//       if (error.failedAssertion) {
-//         return undefined;
-//       } else {
-//         throw new GraphQLError(`guard.auth.error`, {
-//           extensions: {
-//             code: 'UNAUTHENTICATED',
-//             argumentName: 'authorization',
-//             http: {
-//               status: 401,
-//             },
-//             data: {
-//               global: true,
-//               workspace: true,
-//             }
-//           },
-//         });
-//       }
-//     }
-//   };
+const auth = async (req, res, next) => {
+  const token = req?.header("Authorization")
+    ? req?.header("Authorization")?.replace("Bearer ", "")
+    : req.cookies["access-token"];
 
-// //   private authBoom = async (token: string): Promise<UserInterface | undefined> => {
-// //     try {
-// //       const verify = await AwsCognito.verifyToken(token);
-// //       const userExists = await new UserService().findOne({
-// //         sub: verify.sub,
-// //       });
-// //       if (!userExists) {
-// //         throw Boom.unauthorized(`guard.auth.error`, token, {
-// //           message: `Token is invalid or user doesn't exits `,
-// //           path: "authorization",
-// //         });
-// //       }
-// //       if (!userExists.emailVerified || !userExists.phoneNumberVerified) {
-// //         return this.verifyUser(userExists.id, token);
-// //       }
-// //       return userExists;
-// //     } catch (error: any) {
-// //       if (error.failedAssertion) {
-// //         return undefined;
-// //       } else {
-// //         throw Boom.unauthorized(`guard.auth.error`, token, {
-// //           message: `Auth failed `,
-// //           path: "authorization",
-// //         });
-// //       }
-// //     }
-// //   };
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
 
-// //   public grantNext = async (req: UserRequestInterface, res: Response, next: NextFunction) => {
-// //     const token = req.headers.authorization as string;
-// //     if (!token) {
-// //       throw Boom.unauthorized("guard.auth.error", token, {
-// //         message: "Auth failed",
-// //         path: "authorization",
-// //       });
-// //     }
-// //     const user = await this.authBoom(token.replace("Bearer ", ""));
-// //     if (!user) {
-// //       throw Boom.unauthorized(`guard.auth.error`, token, {
-// //         message: `Auth Failed`,
-// //         path: "authorization",
-// //       });
-// //     }
-// //     req.user = user;
-// //     next();
-// //   };
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Your Token Is Expired." });
+    }
 
-// }
+    const userData = await user.findOne({
+      where: { id: decoded.id },
+    });
+    
+    req.user = decoded;
+    next();
+  } 
+  catch (err) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
+};
 
-// const guard = Guard.get();
-
-// export { guard as Guard };
+module.exports = auth;
